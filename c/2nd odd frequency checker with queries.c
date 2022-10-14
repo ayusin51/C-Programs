43b840b0aeec602ecf9ba5a99c3493751042fd89
giant531 <32810230+giant531@users.noreply.github.com>
2022-10-14 19:25:53 +0530

    Update 2nd odd frequency checker with queries.c

--- c/2nd odd frequency checker with queries.c
+++ c/2nd odd frequency checker with queries.c
@@ -6,34 +6,34 @@
 	{
 		int t,max=100;
 		printf("Enter test cases : ");
-		scanf("%d",&t);
+		scanf("%d", &t);
 		while(t--)
 		{
 			int size,i,flag=0;
 			printf("\nEnter size of new array : ");
-			scanf("%d",&size);
+			scanf("%d", &size);
 			int arr[size],arr2[100]={0};
-			for(i=0;i<size;i++)
+			for(i = 0; i < size; i++)
 			{
 			    printf("Enter array elt. : ");
-				scanf("%d",&arr[i]);
+				scanf("%d", &arr[i]);
 				arr2[arr[i]]++;
 			}
 			int q;
 			printf("Enter no.of queries : ");
-			scanf("%d",&q);
+			scanf("%d", &q);
 			while(q--)
 			{
 				int n;
 				printf("Enter query : ");
-				scanf("%d",&n);
-				if(arr2[n]&1)
+				scanf("%d", &n);
+				if(arr2[n] & 1)
 				{
 					printf(" YES\n");
-				}else if(arr2[n]==0)
+				} else if(arr2[n] == 0)
 				{
 					printf(" -1\n");
-				}else
+				} else
 				{
 					printf(" NO\n");
 				}
