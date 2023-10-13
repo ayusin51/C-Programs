2aa0ab4c283a1ee64116cf150cad7d6022355028
giant531 <32810230+giant531@users.noreply.github.com>
2023-10-13 11:45:56 +0530

    Update bisctn_in_deci_using_fabs.c

--- NMST/bisctn_in_deci_using_fabs.c
+++ NMST/bisctn_in_deci_using_fabs.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #define fun(x) pow(x,3)-(4*x)-9
-void main()
-{
+
+void main() {
 float a,b,X,Y;
 float mid;
 //for finding the interval start from 0,1
@@ -9,6 +9,7 @@ a=0;
 b=1; //to check the intermediate property,find out function
 X=fun(a);
 Y=fun(b);
+    
 while((fun(a))*(fun(b))>0)//if positive then a=1,b=2 and so on
     {
     a++;
@@ -19,16 +20,12 @@ while((fun(a))*(fun(b))>0)//if positive then a=1,b=2 and so on
  mid=(a+b)/2;
  printf("\n The first approximate root of fun(x) is %f",mid); //for second approx find fun
 
-while(fabs(a-b)>0.0001)
-{
-    if((fun(a))*(fun(mid))<0)
-    {
+while(fabs(a-b)>0.0001) {
+    if((fun(a))*(fun(mid))<0) {
         a=a;
         b=mid;
         printf("\n Now interval is [%f,%f]",a,mid);
-    }
-    else
-    {
+    } else {
     a=mid;
     b=b;
     printf("\n Now interval is [%f,%f]",mid,b);
@@ -36,5 +33,5 @@ while(fabs(a-b)>0.0001)
     mid=(a+b)/2;
     printf("\n  approximate root is %f",mid);
 
-}
+    }
 }
